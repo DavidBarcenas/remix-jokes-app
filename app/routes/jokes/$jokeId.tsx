a82ec83b5277d21af3a4b7763371e7f1faa8f10b
David Barcenas <daveepro@outlook.com>
2021-12-21 21:02:15 -0600

    get a random joke from prisma

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,20 +1,22 @@
+import { Link, useLoaderData } from 'remix';
 import { z } from 'zod';
-import { useLoaderData } from 'remix';
 import type { LoaderFunction } from 'remix';
-import type { Joke } from '@prisma/client';
-import { db } from '~/utils/db.server';
 
-type LoaderData = { joke: Pick<Joke, 'content'> | null };
+import { db } from '~/utils/db.server';
+import type { Joke } from '@prisma/client';
+
+type LoaderData = { joke: Pick<Joke, 'content' | 'name'> | null };
 
 const LoaderDataSchema = z.object({
   joke: z.object({
     content: z.string(),
+    name: z.string(),
   }),
 });
 
 export const loader: LoaderFunction = async ({ params }) => {
   const joke = await db.joke.findUnique({
-    select: { content: true },
+    select: { content: true, name: true },
     where: { id: params.jokeId },
   });
 
@@ -31,6 +33,7 @@ export default function JokeRoute() {
     <div>
       <p>Here&apos;s your hilarious joke:</p>
       <p>{data.joke?.content}</p>
+      <Link to=".">{data.joke?.name} Permalink</Link>
     </div>
   );
 }
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,43 @@
+import { Link, useLoaderData } from 'remix';
+import { z } from 'zod';
+import type { LoaderFunction } from 'remix';
+
+import { db } from '~/utils/db.server';
+import type { Joke } from '@prisma/client';
+
+type LoaderData = { randomJoke: Joke };
+
+const LoaderDataSchema = z.object({
+  randomJoke: z.object({
+    id: z.string(),
+    content: z.string(),
+    name: z.string(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+  }),
+});
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  const data: LoaderData = { randomJoke };
+  return data;
+};
+
 export default function JokesIndexRoute() {
+  const data = LoaderDataSchema.parse(useLoaderData<LoaderData>());
+
   return (
     <div>
       <p>Here&apos;s a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>
+        &quot;{data.randomJoke.name}&quot; Permalink
+      </Link>
     </div>
   );
 }
