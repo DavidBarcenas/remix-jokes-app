180351b86490210c2971b645c257f413d131b8b1
David Barcenas <daveepro@outlook.com>
2021-12-21 19:46:39 -0600

    get singular jokes

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,36 @@
+import { z } from 'zod';
+import { useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import type { Joke } from '@prisma/client';
+import { db } from '~/utils/db.server';
+
+type LoaderData = { joke: Pick<Joke, 'content'> | null };
+
+const LoaderDataSchema = z.object({
+  joke: z.object({
+    content: z.string(),
+  }),
+});
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    select: { content: true },
+    where: { id: params.jokeId },
+  });
+
+  if (!joke) throw new Error('Joke not found');
+
+  const data: LoaderData = { joke };
+  return data;
+};
+
 export default function JokeRoute() {
+  const data = LoaderDataSchema.parse(useLoaderData<LoaderData>());
+
   return (
     <div>
       <p>Here&apos;s your hilarious joke:</p>
-      <p>
-        Why don&apos;t you find hippopotamuses hiding in trees? They&apos;re
-        really good at it.
-      </p>
+      <p>{data.joke?.content}</p>
     </div>
   );
 }
